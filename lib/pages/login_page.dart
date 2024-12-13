f45f1f2b2b2bb8006b5a3f60f89cdba365e7c08c
Geoffrey Erastus <80622213+Jeffkent01coder@users.noreply.github.com>
2024-12-13 16:19:00 +0300

    logon ui

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -15,16 +15,24 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: _primaryColor,
       body: Align(
-        alignment: Alignment.center,
-       child:  Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              _avaterWidget(),
-            ],
-          ),
-      ),
+          alignment: Alignment.center,
+          child: Container(
+            height: _deviceHeight * 0.60,
+            width: _deviceWidth,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                _avaterWidget(),
+                SizedBox(height: _deviceHeight * 0.05),
+                _emailTextField(),
+                _passwordTextField(),
+                SizedBox(height: _deviceHeight * 0.10),
+                _loginButton(),
+              ],
+            ),
+          )),
     );
   }
 
@@ -41,5 +49,58 @@ class LoginPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Widget _emailTextField() {
+    return Container(
+      width: _deviceWidth * 0.70,
+      child: TextField(
+        cursorColor: Colors.white,
+        autocorrect: false,
+        style: TextStyle(color: Colors.white),
+        decoration: InputDecoration(
+          hintText: "Jeff.cole@gmail.com",
+          hintStyle: TextStyle(color: Colors.white),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _passwordTextField() {
+    return Container(
+      width: _deviceWidth * 0.70,
+      child: TextField(
+        obscureText: true,
+        cursorColor: Colors.white,
+        autocorrect: false,
+        style: TextStyle(color: Colors.white),
+        decoration: InputDecoration(
+          hintText: "Password",
+          hintStyle: TextStyle(color: Colors.white),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _loginButton() {
+    return MaterialButton(
+        minWidth: _deviceWidth * 0.38,
+        height: _deviceWidth * 0.10,
+        color: Colors.white,
+        child: Text(
+          "Login",
+          style: TextStyle(
+              fontSize: 16, color: _primaryColor, fontWeight: FontWeight.bold),
+        ),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25.0),
+            side: BorderSide(color: Colors.white)),
+        onPressed: () {});
   }
 }
